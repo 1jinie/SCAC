d32caf436003ce8fc8eef985def458c4c4322e5b
김수영 <syk6591@naver.com>
2026-07-10 11:37:52 +0900

    feat:SignUp 시나리오에 맞게 수정

--- scac-front/src/pages/Signup/Signup.jsx
+++ scac-front/src/pages/Signup/Signup.jsx
@@ -44,7 +44,7 @@ function SignUpPage() {
       alert(
         '회원가입이 성공적으로 완료되었습니다! 로그인 페이지로 이동합니다.',
       );
-      navigate('/login'); // 가입 성공 시 로그인 페이지로 안내
+      navigate('/'); // 가입 성공 시 메인 페이지로 복귀
     } else {
       setErrorMessage(
         '회원가입에 실패했습니다. 이미 등록된 번호인지 확인해 주세요.',
