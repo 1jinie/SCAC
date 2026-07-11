13629bd1848ed17188195e6baa74f6df92daa340
JH1234567891234567891 <sss03527@gmail.com>
2026-07-11 19:30:39 +0900

    fix:로그인 페이지 연결에러 처리

--- scac-front/src/store/authStore.js
+++ scac-front/src/store/authStore.js
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { postLogin, postLogout } from '../api/authApi';
+import { postLogin, postLogout, postSignUp } from '../api/authApi';
 
 export const useAuthStore = create((set) => ({
   // --- 상태 (State) ---
@@ -27,34 +27,37 @@ export const useAuthStore = create((set) => ({
     }
   },
 
-  /* 로그아웃 처리 액션 - 상태를 초기화하여 출입 권한을 회수 */
-  logout: async () => {
-    try {
-      await postLogout();
-    } catch (error) {
-      console.error('Store Logout Error:', error);
-    } finally {
-      // 에러가 나더라도 클라이언트 상태는 안전하게 리셋
-      set({
-        isLoggedIn: false,
-        userRole: 'GUEST',
-        memberId: null,
-      });
-    }
-  },
+  // /* 로그아웃 처리 액션 - 상태를 초기화하여 출입 권한을 회수 */
 
-  /* 회원가입 처리 액션 */
-  signUp: async (userData) => {
-    try {
-      // authApi의 postSignUp 함수를 호출하여 서버에 전송
-      await postSignUp(userData);
+  // 현재는 에러가 나서 주석처리 해둡니다 필요시 주석해제 해주세요.
 
-      // 회원가입 성공 시 화면(Component)에 성공 신호를 리턴
-      return { success: true };
-    } catch (error) {
-      console.error('Store SignUp Error:', error);
-      // 서버에서 에러 메시지가 올 경우 화면에 뿌려줄 수 있도록 에러 객체 반환
-      return { success: false, error };
-    }
-  },
+  // logout: async () => {
+  //   try {
+  //     await postLogout();
+  //   } catch (error) {
+  //     console.error('Store Logout Error:', error);
+  //   } finally {
+  //     // 에러가 나더라도 클라이언트 상태는 안전하게 리셋
+  //     set({
+  //       isLoggedIn: false,
+  //       userRole: 'GUEST',
+  //       memberId: null,
+  //     });
+  //   }
+  // },
+
+  // /* 회원가입 처리 액션 */
+  // signUp: async (userData) => {
+  //   try {
+  //     // authApi의 postSignUp 함수를 호출하여 서버에 전송
+  //     await postSignUp(userData);
+
+  //     // 회원가입 성공 시 화면(Component)에 성공 신호를 리턴
+  //     return { success: true };
+  //   } catch (error) {
+  //     console.error('Store SignUp Error:', error);
+  //     // 서버에서 에러 메시지가 올 경우 화면에 뿌려줄 수 있도록 에러 객체 반환
+  //     return { success: false, error };
+  //   }
+  // },
 }));
