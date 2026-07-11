c3ac871e18b327ce4dd7687f9fb2105adb4fa047
JH1234567891234567891 <sss03527@gmail.com>
2026-07-11 23:36:49 +0900

    docs:worklog와 README 수정

--- scac-front/src/index.js
+++ scac-front/src/index.js
@@ -4,12 +4,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import './styles/reset.css';
+import './styles/common.css';
+import './styles/global.css';
+import './styles/variables.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
