4886b35a7ccff83b80e9156bd9fae66a71ccf580
JH1234567891234567891 <sss03527@gmail.com>
2026-07-13 10:36:46 +0900

    feat:이용권 더미데이터 추가 및 axiosInstance추가

--- scac-front/src/api/userApi.js
+++ scac-front/src/api/userApi.js
@@ -1,9 +1,9 @@
-import axios from './axios';
+import axiosInstance from './axiosInstance.js';
 
 /* 마이페이지용 */
 export const getUserProfile = async (memberId) => {
   try {
-    const response = await axios.get(`/api/users/${memberId}`);
+    const response = await axiosInstance.get(`/api/users/${memberId}`);
     return response.data;
   } catch (error) {
     console.error('Get User Profile Error:', error);
@@ -14,7 +14,7 @@ export const getUserProfile = async (memberId) => {
 /* 회원이 보유한 활성화된 이용권 목록 조회 */
 export const getUserActiveTickets = async (memberId) => {
   try {
-    const response = await axios.get(`/api/users/${memberId}/tickets`);
+    const response = await axiosInstance.get(`/api/users/${memberId}/tickets`);
     return response.data;
   } catch (error) {
     console.error('Get Active Tickets Error:', error);
@@ -25,7 +25,7 @@ export const getUserActiveTickets = async (memberId) => {
 /* 관리자용 회원 검색 */
 export const getUserByPhone = async (phoneNumber) => {
   try {
-    const response = await axios.get('/api/users/search', {
+    const response = await axiosInstance.get('/api/users/search', {
       params: {
         phone_number: phoneNumber,
       },
