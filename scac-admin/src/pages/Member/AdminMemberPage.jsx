aed8878f676a6a35c8fc93ae221938b8e38f00aa
JH1234567891234567891 <sss03527@gmail.com>
2026-07-21 04:34:51 +0900

    회원관리페이지와 메모페이지 경로 추가

---
+++ scac-admin/src/pages/Member/AdminMemberPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AdminMemberPage() {
+  return <div>AdminMemberPage</div>;
+}
---
+++ scac-admin/src/pages/Memo/AdminMemoPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AdminMemoPage() {
+  return <div>AdminMemoPage</div>;
+}
