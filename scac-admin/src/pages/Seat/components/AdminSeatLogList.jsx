89d12b3a8dfacc6c28affa87294db0fc30b217b3
1jinie <wkddnjswlsn@naver.com>
2026-07-21 17:51:45 +0900

    fix: 관리자 좌석 undefined 버그 수정

--- scac-admin/src/pages/Seat/components/AdminSeatLogList.jsx
+++ scac-admin/src/pages/Seat/components/AdminSeatLogList.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useMemo, useState } from 'react';
-import Pagination from '../../../components/common/Pagination';
+import { useEffect, useMemo, useState } from "react";
+import Pagination from "../../../components/common/Pagination";
 
 export default function AdminSeatLogList({ logs, selectedSeat }) {
   const [currentPage, setCurrentPage] = useState(1);
@@ -10,11 +10,11 @@ export default function AdminSeatLogList({ logs, selectedSeat }) {
     return logs.slice(startIndex, startIndex + PAGE_SIZE);
   }, [logs, currentPage]);
   const ACTION_LABEL = {
-    CHECK_IN: '입실',
-    CHECK_OUT: '퇴실',
-    AUTO_CHECKOUT: '자동퇴실',
-    FORCE_CHECKOUT: '강제퇴실',
-    STATUS_CHANGE: '상태변경',
+    CHECK_IN: "입실",
+    CHECK_OUT: "퇴실",
+    AUTO_CHECKOUT: "자동퇴실",
+    FORCE_CHECKOUT: "강제퇴실",
+    STATUS_CHANGE: "상태변경",
   };
 
   useEffect(() => {
@@ -29,8 +29,8 @@ export default function AdminSeatLogList({ logs, selectedSeat }) {
 
           <p>
             {selectedSeat
-              ? `${selectedSeat.seatNumber}번 좌석 이용 내역`
-              : '전체 좌석 이용 내역'}
+              ? `${selectedSeat}번 좌석 이용 내역`
+              : "전체 좌석 이용 내역"}
           </p>
         </div>
       </div>
@@ -60,7 +60,7 @@ export default function AdminSeatLogList({ logs, selectedSeat }) {
 
                   <td>{log.description}</td>
 
-                  <td>{log.phone_number || '-'}</td>
+                  <td>{log.phone_number || "-"}</td>
                 </tr>
               ))}
             </tbody>
