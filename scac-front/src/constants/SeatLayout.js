c619919dfd4a6237ce9211acaf14a9378848f8c2
1jinie <wkddnjswlsn@naver.com>
2026-07-22 14:35:46 +0900

    chore: seat엔터티의 db데이터와 ui데이터 분리

---
+++ scac-front/src/constants/SeatLayout.js
@@ -0,0 +1,58 @@
+export const seatLayouts = [
+  // 좌석
+  { id: 1, type: 'seat', x: 30, y: 30 },
+  { id: 2, type: 'seat', x: 200, y: 30 },
+  { id: 3, type: 'seat', x: 370, y: 30 },
+  { id: 4, type: 'seat', x: 540, y: 30 },
+  { id: 5, type: 'seat', x: 710, y: 30 },
+
+  { id: 6, type: 'seat', x: 30, y: 230 },
+  { id: 7, type: 'seat', x: 170, y: 230 },
+  { id: 8, type: 'seat', x: 310, y: 230 },
+  { id: 9, type: 'seat', x: 450, y: 230 },
+  { id: 10, type: 'seat', x: 590, y: 230 },
+
+  { id: 11, type: 'seat', x: 30, y: 370 },
+  { id: 12, type: 'seat', x: 170, y: 370 },
+  { id: 13, type: 'seat', x: 310, y: 370 },
+  { id: 14, type: 'seat', x: 450, y: 370 },
+  { id: 15, type: 'seat', x: 590, y: 370 },
+
+  { id: 16, type: 'seat', x: 30, y: 570 },
+  { id: 17, type: 'seat', x: 170, y: 570 },
+  { id: 18, type: 'seat', x: 310, y: 570 },
+  { id: 19, type: 'seat', x: 450, y: 570 },
+  { id: 20, type: 'seat', x: 590, y: 570 },
+
+  { id: 21, type: 'seat', x: 30, y: 710 },
+  { id: 22, type: 'seat', x: 170, y: 710 },
+  { id: 23, type: 'seat', x: 310, y: 710 },
+  { id: 24, type: 'seat', x: 450, y: 710 },
+  { id: 25, type: 'seat', x: 590, y: 710 },
+
+  // 스터디룸
+  {
+    id: 101,
+    type: 'room',
+    x: 30,
+    y: 932,
+    width: 258,
+    height: 360,
+  },
+  {
+    id: 102,
+    type: 'room',
+    x: 288,
+    y: 932,
+    width: 258,
+    height: 360,
+  },
+  {
+    id: 103,
+    type: 'room',
+    x: 546,
+    y: 932,
+    width: 344,
+    height: 360,
+  },
+];
