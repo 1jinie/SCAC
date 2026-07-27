7b1ca168dd3d615893c83391ea0f0dbbde84c83b
김수영 <syk6591@naver.com>
2026-07-27 15:23:59 +0900

    feat : EnableWebSecurity 추가

--- scac-back/src/main/java/com/scac/global/config/SecurityConfig.java
+++ scac-back/src/main/java/com/scac/global/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -16,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
   @Value("${app.frontend-url}")
