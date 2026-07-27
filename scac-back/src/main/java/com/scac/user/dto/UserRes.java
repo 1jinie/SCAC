9402ed175416be183e560669ca6821532143646d
김수영 <syk6591@naver.com>
2026-07-27 17:19:57 +0900

    feat : UserRes file 구현

---
+++ scac-back/src/main/java/com/scac/user/dto/UserRes.java
@@ -0,0 +1,36 @@
+package com.scac.user.dto;
+
+import com.scac.global.enums.UserRole;
+import com.scac.global.enums.UserStatus;
+import com.scac.user.entity.User;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record UserRes(
+
+        Long userId,
+        String phoneNumber,
+        Boolean isMember,
+        UserRole role,
+        UserStatus userStatus,
+        LocalDate penaltyEndDate,
+        LocalDateTime createdAt
+
+) {
+
+    /**
+     * Entity -> Response DTO
+     */
+    public static UserRes from(User user) {
+        return new UserRes(
+                user.getId(),
+                user.getPhoneNumber(),
+                user.getIsMember(),
+                user.getRole(),
+                user.getUserStatus(),
+                user.getPenaltyEndDate(),
+                user.getCreatedAt()
+        );
+    }
+}
