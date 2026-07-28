ef6876e787e950b5a6e6c472d57c111f948d0067
1jinie <wkddnjswlsn@naver.com>
2026-07-28 14:45:27 +0900

    feat:입실 기능에 티켓 확인 및 시간 차감 로직 추가

--- scac-back/src/main/java/com/scac/ticketusage/repository/TicketUsageRepository.java
+++ scac-back/src/main/java/com/scac/ticketusage/repository/TicketUsageRepository.java
@@ -1,9 +1,13 @@
 package com.scac.ticketusage.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.scac.global.enums.TicketUsageStatus;
 import com.scac.ticketusage.entity.TicketUsage;
 
 public interface TicketUsageRepository extends JpaRepository<TicketUsage,Long>{
-
+    Optional<TicketUsage> findFirstByUserIdAndStatusInOrderByCreatedAtDesc(Long userId, List<TicketUsageStatus> statuses);
 }
