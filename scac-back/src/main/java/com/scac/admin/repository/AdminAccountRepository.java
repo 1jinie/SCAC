5ad903d981b333b363e1eb3a9cf91fb2ded798b5
김수영 <syk6591@naver.com>
2026-07-29 15:35:03 +0900

    feat : adminAccountRepository 구현

---
+++ scac-back/src/main/java/com/scac/admin/repository/AdminAccountRepository.java
@@ -0,0 +1,27 @@
+package com.scac.admin.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.scac.admin.entity.AdminAccount;
+import com.scac.admin.entity.AdminRole;
+
+public interface AdminAccountRepository
+        extends JpaRepository<AdminAccount, Long> {
+
+    /**
+     * 로그인 ID 조회
+     */
+    Optional<AdminAccount> findByLoginId(String loginId);
+
+    /**
+     * 로그인 ID 중복 검사
+     */
+    boolean existsByLoginId(String loginId);
+
+    /**
+     * 권한별 관리자 조회
+     */
+    java.util.List<AdminAccount> findByRole(AdminRole role);
+}
