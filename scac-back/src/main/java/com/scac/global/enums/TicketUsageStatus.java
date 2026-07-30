62e07112d864668b0a7a78b0bbe9c681696bf6fe
김수영 <syk6591@naver.com>
2026-07-30 14:59:09 +0900

    fix : enum 수정

--- scac-back/src/main/java/com/scac/global/enums/TicketUsageStatus.java
+++ scac-back/src/main/java/com/scac/global/enums/TicketUsageStatus.java
@@ -2,7 +2,8 @@ package com.scac.global.enums;
 
 public enum TicketUsageStatus {
     READY,  // 이용권 결제 후 대기상태
-    ACTIVE,     // 사용을 시작했으며 아직 이용 가능
-    CANCELED,   // 결제 취소
+    USING,  // 이용권 사용중
     EXPIRED,    // 시간 소진 또는 기간 만료
+
+ 
 }
