751ae9dc3d09b5b12d7fa5114f00714a9142b970
JH1234567891234567891 <sss03527@gmail.com>
2026-07-30 10:19:04 +0900

    fix:백단 페이징한거 일단 주석처리

--- scac-back/src/main/java/com/scac/device/repository/DeviceLogRepository.java
+++ scac-back/src/main/java/com/scac/device/repository/DeviceLogRepository.java
@@ -3,8 +3,8 @@ package com.scac.device.repository;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
-import org.springframework.data.domain.Page;
+// import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+// import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.scac.device.entity.DeviceLog;
@@ -20,7 +20,8 @@ public interface DeviceLogRepository extends JpaRepository<DeviceLog, Long> {
   // 특정 장치의 가장 최신 상태
   Optional<DeviceLog> findFirstByDeviceNameOrderByCreatedAtDescLogIdDesc(String deviceName);
 
-  Page<DeviceLog> findByDeviceNameOrderByCreatedAtDescLogIdDesc(String deviceName, Pageable pageable);
+  // Page<DeviceLog> findByDeviceNameOrderByCreatedAtDescLogIdDesc(String
+  // deviceName, Pageable pageable);
 
   boolean existsByDeviceName(String deviceName);
 
