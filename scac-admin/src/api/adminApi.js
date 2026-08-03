bc79fb4f7754c9a1fb096f9ed719c512fcc07954
김수영 <syk6591@naver.com>
2026-08-03 18:05:03 +0900

    feat : 데이터 추출 가공 로직 추가

--- scac-admin/src/api/adminApi.js
+++ scac-admin/src/api/adminApi.js
@@ -1,13 +1,22 @@
 import axiosInstance from "./axiosInstance.js";
 
 export const adminApi = {
-  login: (data) => axiosInstance.post("/api/admin/auth/login", data),
+  login: async (data) => {
+    const response = await axiosInstance.post("/api/admin/auth/login", data);
+    return response.data; // 로그인 응답은 토큰 저장을 위해 ApiResponse 전체 전달
+  },
 
   //refresh () 작성 예정
 
-  logout: () => axiosInstance.post("/api/admin/auth/logout"),
+  logout: async () => {
+    const response = await axiosInstance.post("/api/admin/auth/logout");
+    return response.data;
+  },
 
-  getDashboardSummary: () => axiosInstance.get("/api/admin/dashboard"),
+  getDashboardSummary: async () => {
+    const response = await axiosInstance.get("/api/admin/dashboard");
+    return response.data.data; // 💡 .data.data로 대시보드 DTO(DashboardRes) 객체 직접 리턴
+  },
 
   getAdminProfile: () => axiosInstance.get("/api/admin/profile"),
 };
