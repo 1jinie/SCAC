407935ca10ad1312406deb74ba0286d1997c48a9
김수영 <syk6591@naver.com>
2026-08-04 18:13:43 +0900

    feat : 스프링 스케줄링 활성화 설정

---
+++ scac-back/src/main/java/com/scac/global/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.scac.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling // 💡 스프링 스케줄링 기능 활성화
+public class SchedulingConfig {
+}
