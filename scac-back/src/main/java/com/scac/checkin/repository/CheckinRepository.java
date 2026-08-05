bfb67847d111662fc1a3767faa4ad8497b86209f
김수영 <syk6591@naver.com>
2026-08-05 17:45:30 +0900

    fix : 단건 반환 시 최신 1건만 반환하도록 다중 레코드 반환 예외 방지 메서드 추가

--- scac-back/src/main/java/com/scac/checkin/repository/CheckinRepository.java
+++ scac-back/src/main/java/com/scac/checkin/repository/CheckinRepository.java
@@ -11,14 +11,19 @@ import com.scac.global.enums.CheckinStatus;
 public interface CheckinRepository extends JpaRepository<Checkin, Long>{
     boolean existsByUserIdAndCheckinStatus(
         Long userId,
-        CheckinStatus checkinStatuses
+        CheckinStatus checkinStatus
     );
 
-    // 사용자 기준 단일 상태 입실 기록 조회
-    Optional<Checkin> findByUserIdAndCheckinStatus(Long userId, CheckinStatus checkinStatus);
+    boolean existsByUserIdAndCheckinStatusIn(
+        Long userId,
+        List<CheckinStatus> statuses
+    );
+
+    // 사용자 기준 단일 상태 입실 기록 최신 1건 조회
+    Optional<Checkin> findFirstByUserIdAndCheckinStatusOrderByCheckinAtDesc(Long userId, CheckinStatus checkinStatus);
     
-    // 사용자 기준 다중 상태 입실 기록 조회
-    Optional<Checkin> findByUserIdAndCheckinStatusIn(Long userId, List<CheckinStatus> statuses);
+    // 사용자 기준 다중 상태 입실 기록 최신 1건 조회
+    Optional<Checkin> findFirstByUserIdAndCheckinStatusInOrderByCheckinAtDesc(Long userId, List<CheckinStatus> statuses);
 
     // 좌석 기준 최신 입실 기록 1건 조회 (Safety 적용)
     Optional<Checkin> findFirstBySeatIdAndCheckinStatusInOrderByCheckinAtDesc(Long seatId, List<CheckinStatus> statuses);
