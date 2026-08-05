7952e91a6c18bd18e1667f756143cb5851581ded
JH1234567891234567891 <sss03527@gmail.com>
2026-08-05 11:51:53 +0900

    fix:admin 회원관리 페이지 버튼 추가 및 연결

--- scac-admin/src/routes/index.jsx
+++ scac-admin/src/routes/index.jsx
@@ -1,25 +1,25 @@
-import { createBrowserRouter } from "react-router-dom";
-import AdminLayout from "../layouts/AdminLayout";
-import AdminDevicePage from "../pages/Device/AdminDevicePage";
-import AdminLogDetailPage from "../pages/Log/AdminLogDetailPage";
-import AdminLogPage from "../pages/Log/AdminLogPage";
-import AdminLoginPage from "../pages/Login/AdminLoginPage";
-import AdminMainPage from "../pages/Main/AdminMainPage";
-import AdminPaymentPage from "../pages/Payment/AdminPaymentPage";
-import AdminReservationPage from "../pages/Reservation/AdminReservationPage";
-import AdminSeatPage from "../pages/Seat/AdminSeatPage";
-import AdminTicketManagePage from "../pages/Ticket/AdminTicketManagePage";
-import AdminErrorPage from "../pages/Error/AdminErrorPage";
-import AdminUserPage from "../pages/User/AdminUserPage";
-import AdminMemoPage from "../pages/Memo/AdminMemoPage";
-import AdminPrivateRoute from "./AdminPrivateRoute";
+import { createBrowserRouter } from 'react-router-dom';
+import AdminLayout from '../layouts/AdminLayout';
+import AdminDevicePage from '../pages/Device/AdminDevicePage';
+import AdminLogDetailPage from '../pages/Log/AdminLogDetailPage';
+import AdminLogPage from '../pages/Log/AdminLogPage';
+import AdminLoginPage from '../pages/Login/AdminLoginPage';
+import AdminMainPage from '../pages/Main/AdminMainPage';
+import AdminPaymentPage from '../pages/Payment/AdminPaymentPage';
+import AdminReservationPage from '../pages/Reservation/AdminReservationPage';
+import AdminSeatPage from '../pages/Seat/AdminSeatPage';
+import AdminTicketManagePage from '../pages/Ticket/AdminTicketManagePage';
+import AdminErrorPage from '../pages/Error/AdminErrorPage';
+import AdminUserPage from '../pages/User/AdminUserPage';
+import AdminMemoPage from '../pages/Memo/AdminMemoPage';
+import AdminPrivateRoute from './AdminPrivateRoute';
 
 const router = createBrowserRouter([
   // ============================
   // Admin
   // ============================
   {
-    path: "/",
+    path: '/',
     element: <AdminLayout />,
     errorElement: <AdminErrorPage />,
     element: (
@@ -31,42 +31,46 @@ const router = createBrowserRouter([
       { index: true, element: <AdminMainPage /> },
 
       {
-        path: "log",
+        path: 'log',
         children: [
           { index: true, element: <AdminLogPage /> },
-          { path: ":logId", element: <AdminLogDetailPage /> },
+          { path: ':logId', element: <AdminLogDetailPage /> },
         ],
       },
 
       {
-        path: "ticket",
+        path: 'ticket',
         children: [{ index: true, element: <AdminTicketManagePage /> }],
       },
 
       {
-        path: "payment",
+        path: 'payment',
         children: [{ index: true, element: <AdminPaymentPage /> }],
       },
 
       {
-        path: "reservation",
+        path: 'reservation',
         children: [{ index: true, element: <AdminReservationPage /> }],
       },
 
       {
-        path: "device",
+        path: 'device',
         children: [{ index: true, element: <AdminDevicePage /> }],
       },
       {
-        path: "user",
+        path: 'user',
         children: [{ index: true, element: <AdminUserPage /> }],
       },
       {
-        path: "memo",
+        path: 'memo',
         children: [{ index: true, element: <AdminMemoPage /> }],
       },
       {
-        path: "seat",
+        path: 'user',
+        children: [{ index: true, element: <AdminUserPage /> }],
+      },
+      {
+        path: 'seat',
         children: [
           {
             index: true,
@@ -78,12 +82,12 @@ const router = createBrowserRouter([
   },
 
   {
-    path: "/login",
+    path: '/login',
     element: <AdminLoginPage />,
     errorElement: <AdminErrorPage />,
   },
   {
-    path: "*",
+    path: '*',
     element: <AdminErrorPage status={404} />,
   },
 ]);
