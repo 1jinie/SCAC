059b5d9809022305245b318ccc8b670b99b380f6
JH1234567891234567891 <sss03527@gmail.com>
2026-08-07 10:17:07 +0900

    docs:work log 업데이트

--- scac-back/src/main/java/com/scac/device/controller/DeviceController.java
+++ scac-back/src/main/java/com/scac/device/controller/DeviceController.java
@@ -1,8 +1,6 @@
 package com.scac.device.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.scac.device.dto.DeviceLogCreateDTO;
 import com.scac.device.dto.DeviceLogResDTO;
-import com.scac.device.dto.DeviceResDTO;
 import com.scac.device.service.DeviceService;
 import com.scac.global.response.ApiResponse;
 
@@ -27,7 +24,7 @@ public class DeviceController {
   // RTOS 이벤트 수신
   @PostMapping("/events")
   public ResponseEntity<ApiResponse<DeviceLogResDTO>> handleDeviceEvent(
-    @Valid @RequestBody DeviceLogCreateDTO form) {
+      @Valid @RequestBody DeviceLogCreateDTO form) {
     DeviceLogResDTO log = deviceService.handleDeviceEvent(form);
 
     return ResponseEntity.ok(ApiResponse.success("장치 이벤트 처리를 완료했습니다.", log));
