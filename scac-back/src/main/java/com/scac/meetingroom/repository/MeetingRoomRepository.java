a766d2e6622adf65166482457da1a23e89b81f03
1jinie <wkddnjswlsn@naver.com>
2026-08-26 14:27:39 +0900

    fix:스터디룸 동시성문제 룸 자체에 비관적 락 적용

--- scac-back/src/main/java/com/scac/meetingroom/repository/MeetingRoomRepository.java
+++ scac-back/src/main/java/com/scac/meetingroom/repository/MeetingRoomRepository.java
@@ -1,9 +1,24 @@
 package com.scac.meetingroom.repository;
 
+import java.util.Optional;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 import com.scac.meetingroom.domain.MeetingRoom;
 
+import jakarta.persistence.LockModeType;
+
 public interface MeetingRoomRepository extends JpaRepository<MeetingRoom, Long> {
     
+    // 예약중인 스터디룸 잠그기
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("""
+                    SELECT r
+                    FROM MeetingRoom r
+                    WHERE r.roomId = :roomId
+                    """)
+    Optional<MeetingRoom> findByIdForUpdate(@Param("roomId") Long roomId);
 }
